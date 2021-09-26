89c8bc5b32922687718e5d56bf316d8b75d8d9ab
SIANI <l.siani92@gmail.com>
2021-09-26 20:32:59 +0200

    Update LocalizerUI.cs

--- Scripts/Runtime/Localization/Components/LocalizerUI.cs
+++ Scripts/Runtime/Localization/Components/LocalizerUI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace LS.Localiser
+namespace LS.Localiser.Runtime
 {
     public class LocalizerUI : MonoBehaviour
     {
